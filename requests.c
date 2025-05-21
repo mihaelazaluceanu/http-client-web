139c3bf336dff727cdd7058a8a002faf12fb6d81
Mihaela Zaluceanu <118905873+mihaelazaluceanu@users.noreply.github.com>
2025-05-21 12:58:16 +0300

    Update requests.c

--- requests.c
+++ requests.c
@@ -1,10 +1,10 @@
-#include <stdlib.h>     /* exit, atoi, malloc, free */
+#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>     /* read, write, close */
-#include <string.h>     /* memcpy, memset */
-#include <sys/socket.h> /* socket, connect */
-#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
-#include <netdb.h>      /* struct hostent, gethostbyname */
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include "helpers.h"
 #include "requests.h"
@@ -15,7 +15,7 @@ char *compute_get_request(char *host, char *url, char *query_params,
     char *message = calloc(BUFLEN, sizeof(char));
     char *line = calloc(LINELEN, sizeof(char));
 
-    // Step 1: write the method name, URL, request params (if any) and protocol type
+    // writes the method name, URL, request params (if any) and protocol type
     if (query_params != NULL) {
         sprintf(line, "GET %s?%s HTTP/1.1", url, query_params);
     } else {
@@ -24,11 +24,11 @@ char *compute_get_request(char *host, char *url, char *query_params,
 
     compute_message(message, line);
 
-    // Step 2: add the host
+    // adds the host
     sprintf(line, "Host: %s", host);
     compute_message(message, line);
 
-    // Step 3 (optional): add headers and/or cookies, according to the protocol format
+    // adds headers and/or cookies, according to the protocol format
     sprintf(line, "User-Agent: Mozilla/5.0");
     compute_message(message, line);
 
@@ -51,7 +51,7 @@ char *compute_get_request(char *host, char *url, char *query_params,
         compute_message(message, line);
     }
 
-    // Step 4: add final new line
+    // adds final new line
     compute_message(message, "");
     return message;
 }
@@ -62,7 +62,7 @@ char *compute_del_request(char *host, char *url, char *query_params,
     char *message = calloc(BUFLEN, sizeof(char));
     char *line = calloc(LINELEN, sizeof(char));
 
-    // Step 1: write the method name, URL, request params (if any) and protocol type
+    // writes the method name, URL, request params (if any) and protocol type
     if (query_params != NULL) {
         sprintf(line, "DELETE %s?%s HTTP/1.1", url, query_params);
     } else {
@@ -71,11 +71,11 @@ char *compute_del_request(char *host, char *url, char *query_params,
 
     compute_message(message, line);
 
-    // Step 2: add the host
+    // adds the host
     sprintf(line, "Host: %s", host);
     compute_message(message, line);
 
-    // Step 3 (optional): add headers and/or cookies, according to the protocol format
+    // adds headers and/or cookies, according to the protocol format
     sprintf(line, "User-Agent: Mozilla/5.0");
     compute_message(message, line);
 
@@ -98,7 +98,7 @@ char *compute_del_request(char *host, char *url, char *query_params,
         compute_message(message, line);
     }
 
-    // Step 4: add final new line
+    // adds final new line
     compute_message(message, "");
     return message;
 }
@@ -110,15 +110,15 @@ char *compute_post_request(char *host, char *url, char *content_type, char **bod
     char *line = calloc(LINELEN, sizeof(char));
     char *body_data_buffer = calloc(LINELEN, sizeof(char));
 
-    // Step 1: write the method name, URL and protocol type
+    // writes the method name, URL and protocol type
     sprintf(line, "POST %s HTTP/1.1", url);
     compute_message(message, line);
     
-    // Step 2: add the host
+    // adds the host
     sprintf(line, "Host: %s", host);
     compute_message(message, line);
 
-    // Step 3: add necessary headers (Content-Type and Content-Length are mandatory)
+    // adds necessary headers (Content-Type and Content-Length are mandatory)
     sprintf(line, "Connection: keep-alive");
     compute_message(message, line);
 
@@ -128,7 +128,7 @@ char *compute_post_request(char *host, char *url, char *content_type, char **bod
     sprintf(line, "Content-Length: %ld", strlen(*body_data));
     compute_message(message, line);
     
-    // Step 4 (optional): add cookies
+    // adds cookies
     if (cookies != NULL) {
        sprintf(line, "Cookie: ");
 
@@ -145,10 +145,10 @@ char *compute_post_request(char *host, char *url, char *content_type, char **bod
         compute_message(message, line);
     }
 
-    // Step 5: add new line at end of header
+    // adds new line at end of header
     compute_message(message, "");
 
-    // Step 6: add the actual payload data
+    // adds the actual payload data
     compute_message(message, *body_data);
 
     memset(line, 0, LINELEN);
