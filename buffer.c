7d6a04d43111c4d5a1e8b1a0f9ad18b8120f1e48
Mihaela Zaluceanu <118905873+mihaelazaluceanu@users.noreply.github.com>
2025-05-21 12:54:15 +0300

    Update buffer.c

--- buffer.c
+++ buffer.c
@@ -1,7 +1,6 @@
 #include "buffer.h"
 
-buffer buffer_init(void)
-{
+buffer buffer_init(void) {
     buffer buffer;
 
     buffer.data = NULL;
@@ -10,8 +9,7 @@ buffer buffer_init(void)
     return buffer;
 }
 
-void buffer_destroy(buffer *buffer)
-{
+void buffer_destroy(buffer *buffer) {
     if (buffer->data != NULL) {
         free(buffer->data);
         buffer->data = NULL;
@@ -20,13 +18,11 @@ void buffer_destroy(buffer *buffer)
     buffer->size = 0;
 }
 
-int buffer_is_empty(buffer *buffer)
-{
+int buffer_is_empty(buffer *buffer) {
     return buffer->data == NULL;
 }
 
-void buffer_add(buffer *buffer, const char *data, size_t data_size)
-{
+void buffer_add(buffer *buffer, const char *data, size_t data_size) {
     if (buffer->data != NULL) {
         buffer->data = realloc(buffer->data, (buffer->size + data_size) * sizeof(char));
     } else {
@@ -38,10 +34,10 @@ void buffer_add(buffer *buffer, const char *data, size_t data_size)
     buffer->size += data_size;
 }
 
-int buffer_find(buffer *buffer, const char *data, size_t data_size)
-{
-    if (data_size > buffer->size)
+int buffer_find(buffer *buffer, const char *data, size_t data_size) {
+    if (data_size > buffer->size) {
         return -1;
+    }
 
     size_t last_pos = buffer->size - data_size + 1;
 
@@ -61,10 +57,10 @@ int buffer_find(buffer *buffer, const char *data, size_t data_size)
     return -1;
 }
 
-int buffer_find_insensitive(buffer *buffer, const char *data, size_t data_size)
-{
-    if (data_size > buffer->size)
+int buffer_find_insensitive(buffer *buffer, const char *data, size_t data_size) {
+    if (data_size > buffer->size) {
         return -1;
+    }
 
     size_t last_pos = buffer->size - data_size + 1;
 
